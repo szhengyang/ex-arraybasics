e148f155ae0f9546c9ae49a8ec2a8fc9a6568538
sdrammis <sdrammis@mit.edu>
2018-02-11 17:08:35 -0500

    Add test and exercise code

--- starter_code/test/test.js
+++ starter_code/test/test.js
@@ -1,20 +1,79 @@
-var expect = require('chai').expect;
-var ex = require('./ex.js').load('./solution.js');
+var should = require('chai').should();
+var ex01   = require('../exercise.js');
 
-describe('Call 1st Function', function() {
-	it('Match Response Message', function() {
-	    expect(ex.first()).to.equal('Success: 1st function works!');
+describe('Array Exercise', function() {
+	it('Change first element of an array', function() {
+		var solution = ['Mango', 'Banana', 'Pear', 'Orange'];
+	    ex01.change().should.eql(solution);
 	});
 });
 
-describe('Call 2nd Function', function() {
-	it('Match Response Message', function() {
-	    expect(ex.second()).to.equal('Success: 2nd function works!');
+describe('Array Exercise', function() {
+	it('Return length of an array', function() {
+	    ex01.length().should.equal(4);
 	});
 });
 
-describe('Call 3rd Function', function() {
-	it('Match Response Message', function() {
-	    expect(ex.third()).to.equal('Success: 3rd function works!');
+describe('Array Exercise', function() {
+	it('Match text', function() {
+	    ex01.concatenate().trim().should.equal(
+	    	'It was the best of times, it was the worst of times'
+	    );
+	});
+});
+
+describe('Array Exercise', function() {
+	it('Adding an element to the end of an array', function() {
+		var solution = ['Banana', 'Pear', 'Orange', 'Mango'];
+	    ex01.addToEnd().should.eql(solution);
+	});
+});
+
+describe('Array Exercise', function() {
+	it('Adding an element to the beginning of an array', function() {
+		var solution = ['Mango', 'Banana', 'Pear', 'Orange'];
+	    ex01.addToBegin().should.eql(solution);
+	});
+});
+
+describe('Array Exercise', function() {
+	it('Deleting the last element of an array', function() {
+		var solution = ['Banana', 'Pear', 'Orange'];
+	    ex01.deleteLast().should.eql(solution);
+	});
+});
+
+describe('Array Exercise', function() {
+	it('Deleting the first element of an array', function() {
+		var solution = ['Pear', 'Orange', 'Mango'];
+	    ex01.deleteFirst().should.eql(solution);
+	});
+});
+
+describe('Array Exercise', function() {
+	it('Adding an element after the first element in an array', function() {
+		var solution = ['Banana', 'Pear', 'Orange', 'Mango'];
+	    ex01.insert().should.eql(solution);
+	});
+});
+
+describe('Array Exercise', function() {
+	it('Concating two arrays', function() {
+		var solution = ['Banana', 'Pear', 'Orange', 'Mango'];
+	    ex01.concat().should.eql(solution);
+	});
+});
+
+describe('Array Exercise', function() {
+	it('Sorting an array alphabetically', function() {
+		var solution = ['Banana', 'Mango', 'Orange', 'Pear'];
+	    ex01.sortAlphabetically().should.eql(solution);
+	});
+});
+
+describe('Array Exercise', function() {
+	it('Sorting an array in a reversed alphabetical order', function() {
+		var solution = ['Pear', 'Orange', 'Mango', 'Banana'];
+	    ex01.reverseSort().should.eql(solution);
 	});
 });
